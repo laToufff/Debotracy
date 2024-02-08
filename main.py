97b5175ed0ce402af9561ce6bd7c569cc6277fd1
laToufff <liam.kern@protonmail.com>
2024-02-08 12:54:46 +0100

    add basic test command "ping"

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-import discord
+import discord as dc
 import os
 
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 
-intents = discord.Intents.default()
-bot = discord.Bot(intents=intents)
+intents = dc.Intents.default()
+bot = dc.Bot(intents=intents, debug_guilds=[int(os.getenv("DEBUG_GUILD"))])
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+class Main (commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'Bot ready as {bot.user.name}!')
+
+    @commands.slash_command()
+    async def ping(self, ctx: dc.ApplicationContext):
+        """This is just a test command."""
+        await ctx.respond('Pong!', ephemeral=True)
+
+bot.add_cog(Main(bot))
 
 bot.run(os.getenv("TOKEN"))
